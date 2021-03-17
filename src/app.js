06dc4fc186dba5bc60340b2340cb9df357e3a5a3
rusne718 <noj.rusne@gmail.com>
2021-03-17 17:21:52 +0000

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -53,6 +53,11 @@ function displayWeatherCondition(response) {
 
     document.querySelector("#icon").setAttribute
     ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+ 
+    document.querySelector("#icon").setAttribute
+    ("alt", response.data.weather[0].main);
+
+    celsiusTemperature = response.data.main.temp;
 
 };
 
@@ -96,3 +101,30 @@ function getCurrentLocation(event) {
 let currentLocationButton = document.querySelector
 ("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let celsiusTemperature = null;
